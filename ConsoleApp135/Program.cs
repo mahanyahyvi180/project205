e4fb8a01f23c3dc199b8349521a0a65be9d4fad4
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-14 10:51:35 +0330

    Add files via upload

---
+++ ConsoleApp135/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp135
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long counter = 0; counter < +10; ++counter)
+            {
+                Console.WriteLine($"{counter}!={Factorial(counter)}");
+                Console.ReadLine();
+            }
+        
+        }
+
+        static long Factorial(long number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
